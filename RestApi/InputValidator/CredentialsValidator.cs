da49fc0c796c2ada7692fbac05fadef07d535928
konvicka <jakub.konvicka@vsb.cz>
2021-05-02 13:34:01 +0200

    Validators string.Empty str definition

---
+++ RestApi/InputValidator/CredentialsValidator.cs
@@ -0,0 +1,115 @@
+﻿using HEAppE.ExtModels.FileTransfer.Models;
+using HEAppE.ExtModels.UserAndLimitationManagement.Models;
+using HEAppE.Utils.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HEAppE.RestApi.InputValidator
+{
+    public class CredentialsValidator : AbstractValidator
+    {
+        private int UsernameMaxLength { get => 50; }
+        public CredentialsValidator(object validationObj) : base(validationObj)
+        {
+        }
+
+        public override ValidationResult Validate()
+        {
+            string message = _validationObject switch
+            {
+                PasswordCredentialsExt ext => ValidateUserPasswordCredentials(ext),
+                OpenIdCredentialsExt ext => ValidateOpenIdCredentials(ext),
+                DigitalSignatureCredentialsExt ext => ValidateDigitalSignatureCredentials(ext),
+                AsymmetricKeyCredentialsExt ext => ValidateAsymmetricKeyCredentials(ext),
+                _ => string.Empty
+            };
+
+            return new ValidationResult(string.IsNullOrEmpty(message), message);
+        }
+
+        private string ValidateAsymmetricKeyCredentials(AsymmetricKeyCredentialsExt credentials)
+        {
+            if (string.IsNullOrEmpty(credentials.Username))
+                _messageBuilder.AppendLine("Username cannot be empty.");
+            else
+            {
+                if (ContainsIllegalCharacters(credentials.Username))
+                    _messageBuilder.AppendLine("Username contains illegal characters.");
+                if (credentials.Username.Length > UsernameMaxLength)
+                    _messageBuilder.AppendLine($"Username is too long, maximal length is {UsernameMaxLength}");
+            }
+
+            if (string.IsNullOrEmpty(credentials.PrivateKey))
+                _messageBuilder.AppendLine("PrivateKey cannot be empty.");
+
+            if (string.IsNullOrEmpty(credentials.PublicKey))
+                _messageBuilder.AppendLine("PublicKey cannot be empty.");
+
+            return _messageBuilder.ToString();
+        }
+
+        private string ValidateDigitalSignatureCredentials(DigitalSignatureCredentialsExt credentials)
+        {
+            if (string.IsNullOrEmpty(credentials.Username))
+                _messageBuilder.AppendLine("Username cannot be empty.");
+            else
+            {
+                if (ContainsIllegalCharacters(credentials.Username))
+                    _messageBuilder.AppendLine("Username contains illegal characters.");
+                if (credentials.Username.Length > UsernameMaxLength)
+                    _messageBuilder.AppendLine($"Username is too long, maximal length is {UsernameMaxLength}");
+            }
+
+            if (string.IsNullOrEmpty(credentials.Noise))
+                _messageBuilder.AppendLine("Noise cannot be empty.");
+            else if (ContainsIllegalCharacters(credentials.Noise))
+                _messageBuilder.AppendLine("Noise contains illegal characters.");
+
+            if (credentials.DigitalSignature == null)
+                _messageBuilder.AppendLine("DigitalSignature cannot be empty.");
+
+            return _messageBuilder.ToString();
+        }
+
+        private string ValidateOpenIdCredentials(OpenIdCredentialsExt credentials)
+        {
+            if (string.IsNullOrEmpty(credentials.OpenIdAccessToken))
+                _messageBuilder.AppendLine("OpenIdAccessToken cannot be empty.");
+            else if (ContainsIllegalCharacters(credentials.OpenIdAccessToken))
+                _messageBuilder.AppendLine("OpenIdAccessToken contains illegal characters.");
+
+            if (string.IsNullOrEmpty(credentials.Username))
+                _messageBuilder.AppendLine("Username cannot be empty.");
+            else
+            {
+                if (ContainsIllegalCharacters(credentials.Username))
+                    _messageBuilder.AppendLine("Username contains illegal characters.");
+                if (credentials.Username.Length > UsernameMaxLength)
+                    _messageBuilder.AppendLine($"Username is too long, maximal length is {UsernameMaxLength}");
+            }
+
+            return _messageBuilder.ToString();
+        }
+
+        private string ValidateUserPasswordCredentials(PasswordCredentialsExt credentials)
+        {
+            if (string.IsNullOrEmpty(credentials.Username))
+                _messageBuilder.AppendLine("Username cannot be empty.");
+            else
+            {
+                if (ContainsIllegalCharacters(credentials.Username))
+                    _messageBuilder.AppendLine("Username contains illegal characters.");
+                if (credentials.Username.Length > UsernameMaxLength)
+                    _messageBuilder.AppendLine($"Username is too long, maximal length is {UsernameMaxLength}");
+            }
+
+            if (string.IsNullOrEmpty(credentials.Password))
+                _messageBuilder.AppendLine("Password cannot be empty.");
+
+
+            return _messageBuilder.ToString();
+        }
+    }
+}
---
+++ RestApi/InputValidator/PathValidator.cs
@@ -0,0 +1,29 @@
+﻿using HEAppE.Utils.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HEAppE.RestApi.InputValidator
+{
+    public class PathValidator : AbstractValidator
+    {
+        public PathValidator(object validationObj) : base(validationObj)
+        {
+        }
+
+        public override ValidationResult Validate()
+        {
+            string message = string.Empty;
+            if(_validationObject is string validationObject)
+            {
+                if(ContainsIllegalCharactersForPath(validationObject))
+                {
+                    message = "Path contains some illegal characters";
+                }
+            }
+
+            return new ValidationResult(string.IsNullOrEmpty(message), message);
+        }
+    }
+}
