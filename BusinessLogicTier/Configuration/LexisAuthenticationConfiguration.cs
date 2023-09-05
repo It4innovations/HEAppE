6c4b7772643ea78215cb2e0137677d08aaf436c9
Lukas Drabek <dra0060@vsb.cz>
2023-09-05 22:41:01 +0200

    default props

--- BusinessLogicTier/Configuration/LexisAuthenticationConfiguration.cs
+++ BusinessLogicTier/Configuration/LexisAuthenticationConfiguration.cs
@@ -7,9 +7,9 @@ public sealed class LexisAuthenticationConfiguration
   public const string configurationPath = "ExternalAuthenticationSettings:LexisAuthenticationConfiguration";
   public const string extendedUserInfoEndpoint = "/api/UserInfo/Extended";
   public string BaseAddress { get; set; } = "http://api.dev.msad.it4i.lexis.tech/userorg";
-  public LexisRoleMapping RoleMapping { get; set; }
-  public string HEAppEGroupNamePrefix { get; set; }
-  public string HEAppEUserPrefix { get; set; }
+  public LexisRoleMapping RoleMapping { get; set; } = new LexisRoleMapping() { Maintainer = "prj_list", Reporter = "prj_read", Submitter = "prj_write" };
+  public string HEAppEGroupNamePrefix { get; set; } = "Lexis_";
+  public string HEAppEUserPrefix { get; set; } = "Lexis_";
 
 
   public Uri BaseAddressUri => new Uri(BaseAddress);
