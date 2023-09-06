e914e925ca8a34c63b1a39cde49195ec55491860
Lukas Drabek <dra0060@vsb.cz>
2023-09-06 09:53:20 +0200

    fix api path

--- BusinessLogicTier/Configuration/LexisAuthenticationConfiguration.cs
+++ BusinessLogicTier/Configuration/LexisAuthenticationConfiguration.cs
@@ -5,7 +5,7 @@ namespace HEAppE.BusinessLogicTier.Configuration;
 public sealed class LexisAuthenticationConfiguration
 {
   public const string configurationPath = "ExternalAuthenticationSettings:LexisAuthenticationConfiguration";
-  public const string extendedUserInfoEndpoint = "/api/UserInfo/Extended";
+  public const string extendedUserInfoEndpoint = "userorg/api/UserInfo/Extended";
   public string BaseAddress { get; set; } = "http://api.dev.msad.it4i.lexis.tech/userorg";
   public LexisRoleMapping RoleMapping { get; set; } = new LexisRoleMapping() { Maintainer = "prj_list", Reporter = "prj_read", Submitter = "prj_write" };
   public string HEAppEGroupNamePrefix { get; set; } = "Lexis_";
