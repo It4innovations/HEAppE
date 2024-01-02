fc3f4aa20384273fed1716b746eda47c1562f2a6
Jakub Konvička <konvicka.g630@gmail.com>
2024-01-02 12:14:34 +0100

    refactor: remove default value
    Signed-off-by: Jakub Konvička <konvicka.g630@gmail.com>

--- DomainObjects/JobManagement/Project.cs
+++ DomainObjects/JobManagement/Project.cs
@@ -36,7 +36,7 @@ namespace HEAppE.DomainObjects.JobManagement
         public bool IsDeleted { get; set; } = false;
 
         [Required]
-        public bool UseAccountingStringForScheduler { get; set; } = true;
+        public bool UseAccountingStringForScheduler { get; set; }
 
         [Required]
         public UsageType UsageType { get; set; } = UsageType.NodeHours;
