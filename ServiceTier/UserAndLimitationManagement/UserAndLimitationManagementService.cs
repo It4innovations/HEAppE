3e0c7674c7c7becd79a72b0f2bb39c7c874b53af
Jakub Konvička <konvicka.g630@gmail.com>
2024-07-17 14:13:29 +0200

    feat: allow to use not valid role for project/group
    Signed-off-by: Jakub Konvička <konvicka.g630@gmail.com>

--- ServiceTier/UserAndLimitationManagement/UserAndLimitationManagementService.cs
+++ ServiceTier/UserAndLimitationManagement/UserAndLimitationManagementService.cs
@@ -207,10 +207,10 @@ namespace HEAppE.ServiceTier.UserAndLimitationManagement
         {
             bool hasRequiredRole = user.AdaptorUserUserGroupRoles.Any(x => x.AdaptorUserRole.ContainedRoleTypes
                                                                     .Any(a => a == requiredUserRole) 
-                                                                        && x.AdaptorUserGroup.ProjectId == projectId
+                                                                           && !overrideProjectValidityCheck || (x.AdaptorUserGroup.ProjectId == projectId
                                                                         && !x.AdaptorUserGroup.Project.IsDeleted
                                                                         && x.AdaptorUserGroup.Project.EndDate >= DateTime.UtcNow
-                                                                        && !x.IsDeleted);
+                                                                        && !x.IsDeleted));
             if (!hasRequiredRole)
             {
                 using var unitOfWork = UnitOfWorkFactory.GetUnitOfWorkFactory().CreateUnitOfWork();
