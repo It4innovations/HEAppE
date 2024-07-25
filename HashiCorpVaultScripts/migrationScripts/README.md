1ab34d9942849901898082e30d0ca51a6defc847
Lukas Drabek <lukas.drabek@vsb.cz>
2024-07-25 20:29:17 +0200

    add updated HashicorpVault scripts

--- HashiCorpVaultScripts/migrationScripts/README.md
+++ HashiCorpVaultScripts/migrationScripts/README.md
@@ -18,7 +18,7 @@ This script will create a CSV format data export, which will be subsequently pro
 
 After successfully backing up the data, you can proceed with the migration of HEAppE to version 4.2.2.
 
-## Step 3: Vault Initialization && run HEAppE
+## Step 3: Vault Initialization
 
 Before importing data into the vault, it is necessary to initialize the vault. Use the `vaultInitToAnsibleFile.sh` script for this.
 
@@ -28,10 +28,10 @@ Before importing data into the vault, it is necessary to initialize the vault. U
 
 ## Step 4: Import Data into the Vault
 
-After successfully initializing the vault, you can import the data using the `importDataToVaultLockedVersion.sh` script.
+After successfully initializing the vault, you can import the data using the `importDataToVault.sh` script.
 
 ```bash
-./importDataToVaultLockedVersion.sh
+./importDataToVault.sh
 ```
 
 The migration is now complete. It is recommended to verify that all data has been correctly imported and that the application is functioning as expected.
---
+++ HashiCorpVaultScripts/migrationScripts/sendFullKeyToVault.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+root_token="$1"
+credentialsId="$2"
+private_key="$3"
+private_key_certificate="$4"
+password="$5"
+private_key_password="$6"
+
+# Check if all required parameters are provided
+if [[ -z $root_token || -z $credentialsId || -z $private_key || -z $private_key_certificate || -z $password || -z $private_key_password ]]; then
+    echo "Missing parameters. Please provide all required parameters."
+    echo "Positional parameters: root_token=$1, credentialsId=${2}, private_key=${3}, private_key_certificate=${4}, password=${5}, private_key_password=${6}"
+    exit 1
+fi
+
+url="http://vaultagent:8100/v1/HEAppE/data/ClusterAuthenticationCredentials/$credentialsId"
+body="{
+  \"data\": {
+    \"Id\": ${credentialsId},
+    \"Password\": \"${password}\",
+    \"PrivateKey\": \"${private_key}\",
+    \"PrivateKeyPassword\": \"${private_key_password}\",
+    \"PrivateKeyCertificate\": \"${private_key_certificate}\"
+  },
+  \"options\": {}
+}"
+
+# Execute the curl command and capture the status code
+status_code=$(docker compose exec -T -e VAULT_TOKEN=$root_token heappe sh -c "curl -o /dev/null -s -w '%{http_code}' -H 'Content-Type: application/json' -d '${body}' '${url}'" &)
+
+# Output the status code
+echo "Status code: $status_code"
+exit $status_code
