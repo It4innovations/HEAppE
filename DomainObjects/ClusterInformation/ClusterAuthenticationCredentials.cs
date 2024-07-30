27e10005e138f29e8add95eeeeee5f2da86add5b
Jakub Konvička <konvicka.g630@gmail.com>
2024-07-30 14:42:11 +0200

    fix: do not convert from base64
    Signed-off-by: Jakub Konvička <konvicka.g630@gmail.com>

--- DomainObjects/ClusterInformation/ClusterAuthenticationCredentials.cs
+++ DomainObjects/ClusterInformation/ClusterAuthenticationCredentials.cs
@@ -82,8 +82,8 @@ namespace HEAppE.DomainObjects.ClusterInformation
 
             var fromBase64PK = Encoding.UTF8.GetString(Convert.FromBase64String(data.PrivateKey));
             var fromBase64PKCert = Encoding.UTF8.GetString(Convert.FromBase64String(data.PrivateKeyCertificate));
-            var passphrase = Encoding.UTF8.GetString(Convert.FromBase64String(data.PrivateKeyPassword));
-            var password = Encoding.UTF8.GetString(Convert.FromBase64String(data.Password));
+            var passphrase = data.PrivateKeyPassword;
+            var password = data.Password;
             _vaultData = data with
             {
                 PrivateKey = fromBase64PK, 
@@ -101,15 +101,15 @@ namespace HEAppE.DomainObjects.ClusterInformation
             }
             var base64PK = Convert.ToBase64String(Encoding.UTF8.GetBytes(PrivateKey.Replace("\r\n", "\n"))); // Replace CRLF with LF
             var base64PKCert = Convert.ToBase64String(Encoding.UTF8.GetBytes(PrivateKeyCertificate.Replace("\r\n", "\n"))); // Replace CRLF with LF
-            var base64Passphrase = Convert.ToBase64String(Encoding.UTF8.GetBytes(PrivateKeyPassphrase));
-            var base64Password = Convert.ToBase64String(Encoding.UTF8.GetBytes(Password));
+            var passphrase = PrivateKeyPassphrase;
+            var password = Password;
             
             return withPrivateKeyEncode ? _vaultData with
             {
                 PrivateKey = base64PK, 
                 PrivateKeyCertificate = base64PKCert,
-                PrivateKeyPassword = base64Passphrase,
-                Password = base64Password
+                PrivateKeyPassword = passphrase,
+                Password = password
             } : _vaultData;
         }
         #endregion
