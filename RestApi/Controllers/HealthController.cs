57711bc3f86920f49be64a2f2eb62912acf2bd76
Jan Trupl <jan.trupl@gmail.com>
2025-09-04 15:34:44 +0200

    Squashed commit of the following:
    commit f777595e15333c70f85bfe28054f5766b9414972
    Author: Jan Trupl <jan.trupl@gmail.com>
    Date:   Tue Sep 2 09:25:47 2025 +0200
    
        #276: move endpoint to /heappe/Health only
    
    commit f4eb5c3f1000417d3b996b4793f4b9680b0ab51c
    Author: Jan Trupl <jan.trupl@gmail.com>
    Date:   Mon Sep 1 19:18:15 2025 +0200
    
        #276: add health checks to swagger - 01

---
+++ RestApi/Controllers/HealthController.cs
@@ -0,0 +1,60 @@
+using HEAppE.Exceptions.External;
+using HEAppE.ExtModels.ClusterInformation.Models;
+using HEAppE.RestApi.InputValidator;
+using HEAppE.RestApiModels.ClusterInformation;
+using HEAppE.ServiceTier.ClusterInformation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace HEAppE.RestApi.Controllers;
+
+/// <summary>
+///     Health Endpoint
+/// </summary>
+[ApiController]
+[Route("heappe/[controller]")]
+[Produces("application/json")]
+public class HealthController : BaseController<HealthController>
+{
+    #region Instances
+
+    private readonly HealthCheckService _healthCheckService;
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    /// <param name="logger">Logger instance</param>
+    /// <param name="cacheProvider">Memory cache instance</param>
+    /// <param name="healthCheckService">Health check service instance</param>
+    public HealthController(ILogger<HealthController> logger, IMemoryCache cacheProvider, HealthCheckService healthCheckService) :
+        base(logger, cacheProvider)
+    {
+        _healthCheckService = healthCheckService;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet]
+    public async Task<ActionResult> Get()
+    {
+        HealthReport report = await this._healthCheckService.CheckHealthAsync();
+        return Ok(HEAppEHealth.DoProcessHealthReport(report));
+    }
+
+    #endregion
+}
