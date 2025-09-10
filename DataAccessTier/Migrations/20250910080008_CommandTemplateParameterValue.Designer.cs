41bdc2373239d8cc4422b719c8f271b45d2a4472
Jan Trupl <jan.trupl@gmail.com>
2025-09-10 10:30:05 +0200

    Increasing column CommandTemplateParameterValue size to 100000. Migration changes column to nvarchar(max), after review it seems it should not pose a problem, but the change is still experimental.

--- DataAccessTier/Migrations/20250910080008_CommandTemplateParameterValue.Designer.cs
+++ DataAccessTier/Migrations/20250910080008_CommandTemplateParameterValue.Designer.cs
@@ -4,6 +4,7 @@ using HEAppE.DataAccessTier;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HEAppE.DataAccessTier.Migrations
 {
     [DbContext(typeof(MiddlewareContext))]
-    partial class MiddlewareContextModelSnapshot : ModelSnapshot
+    [Migration("20250910080008_CommandTemplateParameterValue")]
+    partial class CommandTemplateParameterValue
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
