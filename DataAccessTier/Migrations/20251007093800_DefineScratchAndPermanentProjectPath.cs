80d6a5e68d401f7dd74a6c8dc7b54bd01ae2bd1e
Jakub Konvička <konvicka.g630@gmail.com>
2025-10-07 11:38:33 +0200

    feat: add new migration
    Signed-off-by: Jakub Konvička <konvicka.g630@gmail.com>

---
+++ DataAccessTier/Migrations/20251007093800_DefineScratchAndPermanentProjectPath.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HEAppE.DataAccessTier.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefineScratchAndPermanentProjectPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LocalBasepath",
+                table: "ClusterProject",
+                newName: "ScratchStoragePath");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PermanentStoragePath",
+                table: "ClusterProject",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PermanentStoragePath",
+                table: "ClusterProject");
+
+            migrationBuilder.RenameColumn(
+                name: "ScratchStoragePath",
+                table: "ClusterProject",
+                newName: "LocalBasepath");
+        }
+    }
+}
