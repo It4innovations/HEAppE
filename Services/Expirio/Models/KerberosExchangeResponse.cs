306b43a2a84ec6da5689edb50a0353dd46d9d09d
João Carias <joao.carias@vsb.cz>
2026-01-22 12:35:43 +0000

    Properties for Json deserialization.

--- Services/Expirio/Models/KerberosExchangeResponse.cs
+++ Services/Expirio/Models/KerberosExchangeResponse.cs
@@ -1,40 +1,35 @@
 using System.ComponentModel;
 using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Services.Expirio.Models;
 
 /// <summary>
 /// Kerberos exchange response
 /// </summary>
-[DataContract(Name = "KerberosExchangeResponse")]
-[Description("Kerberos Exchange response")]
 public class KerberosExchangeResponse
 {
     /// <summary>
     /// FileName
     /// </summary>
-    [DataMember(Name = "FileName")]
-    [Description("FileName")]
-    public required string FileName { get; set; }
+    [JsonPropertyName("fileName")]
+    public string? FileName { get; set; }
 
     /// <summary>
     /// Content
     /// </summary>
-    [DataMember(Name = "Content")]
-    [Description("Content")]
-    public required string Content { get; set; }
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
 
     /// <summary>
     /// Size
     /// </summary>
-    [DataMember(Name = "Size")]
-    [Description("Size")]
-    public required int Size { get; set; }
+    [JsonPropertyName("size")]
+    public int? Size { get; set; }
 
     /// <summary>
     /// Timestamp
     /// </summary>
-    [DataMember(Name = "Timestamp")]
-    [Description("Timestamp")]
-    public required string Timestamp { get; set; }
+    [JsonPropertyName("timestamp")]
+    public string? Timestamp { get; set; }
 }
