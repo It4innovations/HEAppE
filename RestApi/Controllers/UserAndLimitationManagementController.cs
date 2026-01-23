8c998164cb1115eee4943767b186513d0a75db29
Jakub Konvička <konvicka.g630@gmail.com>
2026-01-23 18:04:50 +0100

    feat: change message
    Signed-off-by: Jakub Konvička <konvicka.g630@gmail.com>

--- RestApi/Controllers/UserAndLimitationManagementController.cs
+++ RestApi/Controllers/UserAndLimitationManagementController.cs
@@ -70,7 +70,7 @@ public class UserAndLimitationManagementController : BaseController<UserAndLimit
         if (JwtTokenIntrospectionConfiguration.IsEnabled || LexisAuthenticationConfiguration.UseBearerAuth)
         {
             _logger.LogInformation("Lexis token authentication is handled by middleware. Returning empty string.");
-            return Ok("bad0head-0000-4000-b000-000000000000");
+            return Ok("HEADER-AUTH-NEEDED");
         }
         var validationResult = new UserAndLimitationManagementValidator(model).Validate();
         if (!validationResult.IsValid) throw new InputValidationException(validationResult.Message);
