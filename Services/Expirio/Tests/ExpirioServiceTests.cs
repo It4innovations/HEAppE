88c2f504f2ebb187efdd7486f4235931f5529524
jocarias <11091450+jocarias@users.noreply.github.com>
2026-01-26 14:26:33 +0000

    Additional test case.

--- Services/Expirio/Tests/ExpirioServiceTests.cs
+++ Services/Expirio/Tests/ExpirioServiceTests.cs
@@ -63,6 +63,35 @@ public class ExpirioServiceTests
         }
     }
 
+    [Fact]
+    public async Task GetKerberosTicket_should_return_not_found()
+    {
+        HttpClient httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri(ExpirioSettings.BaseUrl),
+            Timeout = TimeSpan.FromSeconds(ExpirioSettings.TimeoutSeconds),
+        };
+        httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+        //TODO: needs to get Expirio test token.
+
+        var expirio = new ExpirioService(httpClient);
+        var request = new KerberosExchangeRequest()
+        {
+            ProviderName = "",
+        };
+
+        var ct = new CancellationToken();
+        try
+        {
+            var ticket = await expirio.ExchangeTokenForKerberosAsync(request, ct);
+        }
+        catch(ExpirioNotFoundException)
+        {
+            Assert.True(true);
+        }
+    }
+
     //TODO: bad request (400 with error body)?
 
     //TODO: server error (500) — ensure retry policy invoked?
