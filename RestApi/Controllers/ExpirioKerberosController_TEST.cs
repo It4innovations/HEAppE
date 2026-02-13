ca3acf7f2a7ced2acad9658252724436d991b150
João Carias <joao.carias@vsb.cz>
2026-02-13 21:29:48 +0000

    Changes to the controller test.

--- RestApi/Controllers/ExpirioKerberosController_TEST.cs
+++ RestApi/Controllers/ExpirioKerberosController_TEST.cs
@@ -31,7 +31,7 @@ public class ExpirioController : ControllerBase
     public async Task<IActionResult> GetKerberosTicket([FromBody] KerberosExchangeRequest request, CancellationToken ct)
     {
         //var ticket = await _expirio.ExchangeTokenForKerberosAsync(request, _httpContextKeys.Context.LEXISToken, ct);
-        var ticket = "BQQAAAAAAAEAAAABAAAABE1FVEEAAAALam9hb19jYXJpYXMAAAABAAAAAQAAAARNRVRBAAAAC2pvYW9fY2FyaWFzAAAAAgAAAAIAAAAETUVUQQAAAAZrcmJ0Z3QAAAAETUVUQQASAAAAIMH0xDIii76rFZ4q3B2mSGgbavuTCG0DJTUVJ97kk5KnaYHmtGmB5rVpgo91AAAAAABgKAAAAAAAAAAAAAAAAAFGYYIBQjCCAT6gAwIBBaEGGwRNRVRBohkwF6ADAgECoRAwDhsGa3JidGd0GwRNRVRBo4IBEjCCAQ6gAwIBEqEDAgEEooIBAASB/eSYNE1/16/11ElBjhKXnQx/LzseT6t6ZBMtSuPxzEHlN91tYFR3yDpFY0WiRHR1nObQ1HDRR2Ausw9xlXzb3kH7zDLOQGQUDvSB4wd7GtuX0W+tqVFslLD2/ndHbvWdtljVk0EjFiv4WEykTUfQ99XH52oknzvRemx3X/2JIPd5YWjYfOmJfBg/o9rDa6R9JgUNDZYntg2Zfp8RsA/BDSop5YqZrSxGoipqHbwWC2OPhXC9ozuRMG2K+rPbPRYHX6sk0VXHKuFKrxJXl7YyYhnCFrSD5kFafkFskOIawIaAlxQY50uClgh2fkFUxIJ6MKkVPXHu0O1RlCQkyPwAAAAA";
+        var ticket = "";
         byte[] expirio_tkt = Convert.FromBase64String(ticket);
         string username = KrbLibSim.AddOrUpdateTicketCache(expirio_tkt);
 
@@ -61,6 +61,42 @@ public class ExpirioController : ControllerBase
 
         using (var client = new SshClient(address, sshConfigSettings, loggerFactory))
         {
+            //////////////////////////////////
+            /*
+            try
+            {
+                Console.WriteLine($"\n\n#############");
+                Console.WriteLine($"# User [{username}] connecting to [{address}]...");
+
+                var watch = Stopwatch.StartNew();
+                // Connect to the SSH client
+                await client.ConnectAsync();
+                watch.Stop();
+
+                string msg = $"--- User [{username}] connection to [{address}] successful! ({watch.ElapsedMilliseconds}ms) ---";
+                string msgExt = new('@', msg.Length);
+                string msgInt = new('-', msg.Length);
+
+                string command = "whoami";
+
+                Console.WriteLine($"User [{username}] on service [{address}] executing remote command: {command}\n...\n");
+                //
+                Renci.SshNet.ConnectionInfo connInfo = new (address, username, [new Renci.SshNet.NoneAuthenticationMethod(username)]);
+                var sshClient = new Renci.SshNet.SshClient(connInfo);
+                //sshClient.Connect();
+                var comm = sshClient.RunCommand(command);
+
+                Console.WriteLine($"{msgInt}\nUser [{username}], service [{address}], stdout: {"line1"}\n{msgInt}\n");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"User [{username}], service [{address}] ### An error occurred: {ex.Message}");
+                Console.WriteLine($"User [{username}], service [{address}] ### StackTrace: {ex.StackTrace}\n");
+            }
+            if(false)
+            /**/
+            ////////////////////////////////
             try
             {
                 Console.WriteLine($"\n\n#############");
@@ -82,8 +118,8 @@ public class ExpirioController : ControllerBase
                     Console.WriteLine($"User [{username}] on service [{address}] executing remote command: {command}\n...\n");
                     using (var process = await client.ExecuteAsync(command))
                     {
-                        (bool isError1, string? line1) = await process.ReadLineAsync();
-                        Console.WriteLine($"{msgInt}\nUser [{username}], service [{address}], stdout: {line1}\n{msgInt}\n");
+                        (bool isError, string? line) = await process.ReadLineAsync();
+                        Console.WriteLine($"{msgInt}\nUser [{username}], service [{address}], stdout: {line}\n{msgInt}\n");
                         //(string? stdout, string? stderr) = await process.ReadToEndAsStringAsync();
                         //Console.WriteLine($"stdout: {stdout}");
                     }
