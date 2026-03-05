bda53489e62ca811b0f8e4fc757db823ed864c6f
João Carias <joao.carias@vsb.cz>
2026-03-05 01:47:07 +0000

    Added switch for different Auth.

--- RestApi/InputValidator/CredentialValidator.cs
+++ RestApi/InputValidator/CredentialValidator.cs
@@ -28,11 +28,46 @@ public class CredentialValidator : AbstractValidator
             _messageBuilder.AppendLine(sessionCodeValidation.Message);
 
         ValidateId(ext.ProjectId, "ProjectId");
-        
         if(ext.AuthType == ClusterAuthenticationCredentialsAuthType.Kerberos)
+        switch(ext.AuthType)
         {
-            if(!string.IsNullOrEmpty(ext.Passphrase))
-                _messageBuilder.AppendLine("Passphrase is not applicable for Kerberos credentials.");
+            case ClusterAuthenticationCredentialsAuthType.Kerberos:
+                if(ext.GenerateNewKey is true)
+                    _messageBuilder.AppendLine("GenerateNewKey is not applicable for Kerberos credentials.");
+                
+                if(!string.IsNullOrEmpty(ext.ProvidedPrivateKey))
+                    _messageBuilder.AppendLine("ProvidedPrivateKey is not applicable for Kerberos credentials.");
+
+                if(!string.IsNullOrEmpty(ext.Passphrase))
+                    _messageBuilder.AppendLine("Passphrase is not applicable for Kerberos credentials.");
+                
+                if(!string.IsNullOrEmpty(ext.Password))
+                    _messageBuilder.AppendLine("Password is not applicable for Kerberos credentials.");
+                
+                break;
+            
+            case ClusterAuthenticationCredentialsAuthType.PasswordAndPrivateKey:
+                if(ext.GenerateNewKey is true && string.IsNullOrEmpty(ext.ProvidedPrivateKey))
+                    _messageBuilder.AppendLine("ProvidedPrivateKey cannot be null or empty when GenerateNewKey is true, for PasswordAndPrivateKey Authentication.");
+
+                if(string.IsNullOrEmpty(ext.Password))
+                    _messageBuilder.AppendLine("Password cannot be null or empty for PasswordAndPrivateKey Authentication.");
+
+                //TODO: what about Passphrase?
+                //if(string.IsNullOrEmpty(ext.Passphrase))
+                //    _messageBuilder.AppendLine("Passphrase cannot be null or empty for PasswordAndPrivateKey Authentication.");
+
+                break;
+
+            case ClusterAuthenticationCredentialsAuthType.PrivateKey:
+                if(ext.GenerateNewKey is true && string.IsNullOrEmpty(ext.ProvidedPrivateKey))
+                    _messageBuilder.AppendLine("ProvidedPrivateKey cannot be null or empty when GenerateNewKey is true, for PrivateKey Authentication.");
+
+                break;
+            
+            default:
+                _messageBuilder.AppendLine($"{ext.AuthType} Authentication type not supported.");
+                break;
         }
 
         return _messageBuilder.ToString();
