eb1323368c06bb6118970a5e7f7e8460dee56a7c
Jakub Konvička <konvicka.g630@gmail.com>
2026-07-13 19:44:14 +0200

    feat: generate database migration for QSchedulerSession.SessionId index

---
+++ DataAccessTier/Migrations/20260713174410_AddQSchedulerSessionIdIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HEAppE.DataAccessTier.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQSchedulerSessionIdIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_QSchedulerSession_SessionId",
+                table: "QSchedulerSession",
+                column: "SessionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_QSchedulerSession_SessionId",
+                table: "QSchedulerSession");
+        }
+    }
+}
