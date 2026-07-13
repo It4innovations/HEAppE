e88b3f317ddfb3dccda6efa67b6ef0c46d3a1d23
Jakub Konvička <konvicka.g630@gmail.com>
2026-07-13 18:54:35 +0200

    opt: only query active tasks in GetTasksByScheduledJobIdAsync repository method

--- DataAccessTier/Repository/JobManagement/JobInformation/SubmittedTaskInfoRepository.cs
+++ DataAccessTier/Repository/JobManagement/JobInformation/SubmittedTaskInfoRepository.cs
@@ -292,7 +292,7 @@ internal class SubmittedTaskInfoRepository : GenericRepository<SubmittedTaskInfo
             .Include(t => t.Specification)
                 .ThenInclude(ts => ts.JobSpecification)
                     .ThenInclude(js => js.Submitter)
-            .Where(t => t.ScheduledJobId == taskPrefix || t.ScheduledJobId == scheduledJobId || t.ScheduledJobId.EndsWith(taskSuffix))
+            .Where(t => (t.ScheduledJobId == taskPrefix || t.ScheduledJobId == scheduledJobId || t.ScheduledJobId.EndsWith(taskSuffix)) && t.State < TaskState.Finished)
             .ToListAsync();
     }
 
