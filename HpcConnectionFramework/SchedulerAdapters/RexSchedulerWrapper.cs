17397fcd6cf7098359290141e88c80acb80a5437
Jakub Konvička <konvicka.g630@gmail.com>
2026-07-15 08:24:03 +0200

    perf: replace Task.Delay(1) with Task.CompletedTask in ReturnConnectionAsync

--- HpcConnectionFramework/SchedulerAdapters/RexSchedulerWrapper.cs
+++ HpcConnectionFramework/SchedulerAdapters/RexSchedulerWrapper.cs
@@ -735,7 +735,7 @@ public class RexSchedulerWrapper : IRexScheduler
     private Task ReturnConnectionAsync(ConnectionInfo schedulerConnection)
     {
         if (_connectionPool == null || schedulerConnection == null || schedulerConnection == DummyConnectionInfo || schedulerConnection.AuthCredentials == null)
-            return Task.Delay(1);
+            return Task.CompletedTask;
         return _connectionPool.ReturnConnectionAsync(schedulerConnection);
     }
 
