8cbc747b65e69a3cca38f1e7f778dd73919f4aa4
Jakub Konvička <konvicka.g630@gmail.com>
2026-07-27 09:03:14 +0200

    fix(dictionary): add missing using System.Reflection namespace and update Swagger API specs

--- RestApi/Controllers/DictionaryController.cs
+++ RestApi/Controllers/DictionaryController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using HEAppE.DomainObjects.ClusterInformation;
 using HEAppE.ExtModels.ClusterInformation.Models;
 using HEAppE.ExtModels.FileTransfer.Models;
