2b2c60ee4616aca9462881f3a7dfecf1b8f95eef
Jakub Konvička <konvicka.g630@gmail.com>
2026-09-07 09:55:12 +0200

    feat: add support for UserOrg API v2 endpoints with configurable strategy

--- ExternalAuthentication/Configuration/LexisAuthenticationConfiguration.cs
+++ ExternalAuthentication/Configuration/LexisAuthenticationConfiguration.cs
@@ -1,10 +1,13 @@
-﻿namespace HEAppE.ExternalAuthentication.Configuration;
+namespace HEAppE.ExternalAuthentication.Configuration;
 
 public class LexisAuthenticationConfiguration
 {
+    public static string ApiVersion { get; set; } = "v2";
     public static bool UseBearerAuth { get; set; } = false;
     public static string ExtendedUserInfoEndpoint { get; set; } = "/api/UserInfo/Extended";
+    public static string ExtendedUserInfoV2Endpoint { get; set; } = "/api/v2/users/me/extended";
     public static string CommandTemplatePermissions { get; set; } = "/api/Heappe/CommandTemplatePermissions/";
+    public static string CommandTemplatePermissionsV2Endpoint { get; set; } = "/api/v2/heappe/command-template-permissions/";
     public static bool CheckCommandTemplatePermissions { get; set; } = false;
     public static string BaseAddress { get; set; }
     public static string EndpointPrefix { get; set; }
