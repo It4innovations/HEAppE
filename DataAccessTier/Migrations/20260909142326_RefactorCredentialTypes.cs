4c017635fc033d08b5b6ec6efae696861030d7f9
João Manuel Da Silva Carias <joao.carias@vsb.cz>
2026-09-11 08:56:03 +0200

    refactor(credentialstype): added migration files.

---
+++ DataAccessTier/Migrations/20260909142326_RefactorCredentialTypes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HEAppE.DataAccessTier.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactorCredentialTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
